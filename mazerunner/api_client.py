9d971b843e9f34a721673daa6f1c3635b7991e94
Yochai Blumenfeld <yochaibl@gmail.com>
2017-08-13 16:12:19 +0300

    change url

--- mazerunner/api_client.py
+++ mazerunner/api_client.py
@@ -599,10 +599,10 @@ class DeploymentGroupCollection(EditableCollection):
             domain=domain,
             deploy_on=deploy_on
         )
-        url = "{}1/".format(self._get_url().replace(self._obj_class.NAME, 'deploy'))
+        url = "{}{}/".format(self._get_url(), 'deploy')
         return self._api_client.api_request(url=url,
-                                             method='put',
-                                             data=data)
+                                            method='post',
+                                            data=data)
 
     def deploy_all(self, location_with_name, os, download_format="ZIP"):
         """
