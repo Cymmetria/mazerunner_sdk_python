a1429909dbecc4440ff1bef4add0e1cbd6bf4244
frankie <frankie@cymmetria.com>
2018-07-15 17:14:02 +0300

    Version update to 1.2.1

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("long_description.txt", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='mazerunner_sdk',
     packages=find_packages(),
-    version='1.1.3',
+    version='1.2.1',
     description='MazeRunner SDK',
+    long_description=long_description,
     author='Cymmetria',
     author_email='publicapi@cymmetria.com',
     url='https://github.com/Cymmetria/mazerunner_sdk_python',
-    download_url='https://github.com/Cymmetria/mazerunner_sdk_python/tarball/1.1.3',
+    download_url='https://github.com/Cymmetria/mazerunner_sdk_python/tarball/1.2.1',
     license='BSD 3-Clause',
     keywords=['cymmetria', 'mazerunner', 'sdk', 'api'],
     install_requires=["argparse==1.2.1",
